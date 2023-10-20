0063f77528df706ce62aa83280356ecb823cdc3f
Tyler <yumenohik@gmail.com>
2023-10-20 11:22:15 +0900

    Episode 15 - Fetching Data with React Query

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import useFetch from "./useFetch";
+import {useQuery} from "react-query";
 
-function Joke(props) {
-    const { data: joke, isLoading, errorMessage } = useFetch('https://official-joke-api.appspot.com/jokes/random');
-    
+function Joke() {
+    // const { data: joke, isLoading, errorMessage } = useFetch('https://official-joke-api.appspot.com/jokes/random');
+
+    const {
+        data: joke,
+        isLoading,
+        isError,
+        error,
+        isSuccess,
+    } = useQuery('joke', fetchJoke, {
+        staleTime: 6000,
+        refetchOnWindowFocus: false,
+    });
+
+    function fetchJoke() {
+        return fetch('https://official-joke-api.appspot.com/jokes/random').then(response =>
+            response.json()
+        );
+    }
+
     return (
         <div>
             <h2>Joke API</h2>
             {isLoading && <div>Loading...</div>}
-            {joke && (<div>{joke.setup + ' ' + joke.punchline}</div>)}
+            {isSuccess && (<div>{joke.setup + ' ' + joke.punchline}</div>)}
 
-            { errorMessage && <div>{errorMessage}</div> }
+            {isError && <div>{error.message}</div>}
         </div>
     );
 }
--- src/Reddit.jsx
+++ src/Reddit.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import useFetch from "./useFetch";
+import {useQuery} from "react-query";
 
 function Reddit() {
-    const { data: posts, isLoading, errorMessage } = useFetch('https://www.reddit.com/r/aww.json');
+    // const { data: posts, isLoading, errorMessage } = useFetch('https://www.reddit.com/r/aww.json');
+
+    const {
+        data: posts,
+        isLoading,
+        isError,
+        error,
+        isSuccess,
+    } = useQuery('posts', fetchPosts, {
+        retry: false,
+    });
+
+    function fetchPosts() {
+        return fetch('https://www.reddit.com/r/aww.jsonx').then(response =>
+            response.json()
+        );
+    }
 
     return (
         <div>
             <h2>Reddit API</h2>
             {isLoading && <div>Loading...</div>}
-            {posts && (
+            {isSuccess && (
                 <ul>
                     {posts.data.children.map(post => (
                         <li key={post.data.id}>
@@ -19,7 +36,7 @@ function Reddit() {
                     ))}
                 </ul>
             )}
-            {errorMessage && <div>{errorMessage}</div>}
+            {isError && <div>{error.message}</div>}
         </div>
     );
 }
